d25e0b3a189807f69e4b14d47d2b522b5f3ac3c6
cmarsa <marcodelossantos@protonmail.com>
2021-05-17 10:54:28 -0500

    Basinhopping with ackley function.

---
+++ optimization/basinhopping/ackley.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+# ackley.py
+
+"""Multimodal objective functions are those
+thst gave multiple optima, such as a global optima
+and manu local optima, or multiple global optima
+with the same objective function output.
+
+The Ackley function is an example of an objective
+function that has a single global optima and multiple local optima
+in which a local search might get stuck.
+"""
+
+from numpy import arange
+from numpy import exp
+from numpy import sqrt
+from numpy import cos
+from numpy import e
+from numpy import pi
+from numpy import meshgrid
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def main():
+    # define range for input
+    r_min, r_max = -5.0, 5.0
+    # sample input range uniformly ay 01.increments
+    xaxis = arange(r_min, r_max, 0.1)
+    yaxis = arange(r_min, r_max, 0.1)
+    # create a mesh from the axis
+    x, y = meshgrid(xaxis, yaxis)
+    # compute targets
+    results = objective(x, y)
+    # create a surface plot
+    figure = plt.figure()
+    axis = figure.gca(projection='3d')
+    axis.plot_surface(x, y, results, cmap='jet')
+    # show the plot
+    plt.show()
+
+
+# ackley function
+def objective(x, y):
+	return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2))) - exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y))) + e + 20
+
+
+if __name__ == '__main__':
+    main()
---
+++ optimization/basinhopping/basin_hopping.py
@@ -0,0 +1,37 @@
+# -*- ooding: utf-8 -*-
+# basin_gopping.py
+
+from numpy import exp
+from numpy import sqrt
+from numpy import cos
+from numpy import e
+from numpy import pi
+from numpy.random import rand
+from scipy.optimize import basinhopping
+
+def main():
+    # define range for input
+    r_min, r_max = -5.0, 5.0
+    # define the starting point as a random sample from
+    # the domain
+    pt = r_min + rand(2) * (r_max- r_min)
+    # perform the basin hopping search
+    result = basinhopping(ackley, pt, stepsize=0.5, niter=200)
+    # summarize the result
+    print(f'status: {result["message"]}')
+    print(f'total evaluations: {result["nfev"]:d}')
+    # evaluate solution
+    solution = result['x']
+    evaluation = ackley(solution)
+    print(f'solution: f({solution}) = {evaluation:.5f}')
+
+
+
+# ackley function
+def ackley(v):
+    x, y = v
+    return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2))) - exp(0.5 * (cos(2 * pi * x) + cos(2 * pi * y))) + e + 20
+
+
+if __name__ == '__main__':
+    main()
